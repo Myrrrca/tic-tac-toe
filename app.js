fb058bf6266d0a43abbf838c82ac2f7f44d9cd6e
myrrrca <damskieiygodnik217@gmail.com>
2023-01-06 08:39:47 +0300

    adfaeaar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const cells = document.querySelectorAll(".cell");
 const board = document.querySelector(".board");
+const aganeBtn = document.querySelector(".new-game-button");
 
 let counter = 0;
 
@@ -8,7 +9,10 @@ const clickOnCell = (cell, index) => {
   let circle = document.createElement("div");
   cross.style.display = "none";
   const currentCell = document.querySelector(`.cell:nth-of-type(${index + 1})`);
-  if (currentCell.classList.contains("plased")) {
+  if (
+    currentCell.classList.contains("cross") ||
+    currentCell.classList.contains("circle")
+  ) {
     alert("This cell already conatins character");
   } else {
     if (counter % 2 == 0) {
@@ -18,7 +22,7 @@ const clickOnCell = (cell, index) => {
       cross.style.position = "absolute";
       cross.style.backgroundColor = "#3e3d49";
       cross.style.display = "block";
-      currentCell.classList.add("plased");
+      currentCell.classList.add("cross");
       currentCell.append(cross);
       ++counter;
     } else {
@@ -28,15 +32,21 @@ const clickOnCell = (cell, index) => {
       circle.style.position = "absolute";
       circle.style.backgroundColor = "#F5B441";
       circle.style.display = "block";
-      currentCell.classList.add("plased");
+      currentCell.classList.add("circle");
       currentCell.append(circle);
       ++counter;
     }
   }
 };
 
+const clickOnNewGameBtn = () => {
+  window.location.reload();
+};
+
 const cellLoop = (cell, index) => {
   cell.addEventListener("click", () => clickOnCell(cell, index));
 };
+
+aganeBtn.addEventListener("click", clickOnNewGameBtn);
 
 cells.forEach(cellLoop);
